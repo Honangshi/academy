e8f53e0c036bc4f3aec6740297294a91517e381d
KimDonghwan <ghskdtl@naver.com>
2022-02-19 17:44:33 +0900

    Update MySQL.h

--- MySQL.h
+++ MySQL.h
@@ -12,6 +12,8 @@ MYSQL_RES* queryResult;
 //긁어온 정보의 1줄
 MYSQL_ROW queryRow;
 
+string targetTable = "Userdata";
+
 bool MySQLInitialize() {
 	if (!(connectedDatebase = mysql_init((MYSQL*)nullptr))) {
 		cout << "Cannot Initialize MySQL" << endl;
@@ -41,12 +43,47 @@ void MySQLClose() {
 	mysql_close(connectedDatebase);
 }
 
-void SaveUser(string id, string color) {
-	string query = "INSERT INTO Userdata(ID, Color) VALUES (\"";
+bool LoadUser(string id) {
+	string query = "SELECT * FROM ";
+	query += targetTable;
+	query += "where ID = \"";
 	query += id;
-	query += "\", \"";
-	query += color;
-	query += "\");";
+	query += "\";";
+	if (mysql_query(connectedDatebase, query.c_str()) != 0) {
+		return false;
+	}
+	//select문은 물어보고 가져오는 것 실제로 어딘가에 저장해야 함
+	queryResult = mysql_store_result(connectedDatebase);
+
+	//한줄로 받아와야함
+	queryRow = mysql_fetch_row(queryResult);
+
+	return true;
+}
+
+void SaveUser(string id, string color) {
+	string query;
+
+	if (LoadUser(id)) {
+		//이미 유저가 있다면 값만 바꿔주면 됨
+		query = "UPDATE ";
+		query += targetTable;
+		query += " SET COLOR=\"";
+		query += color;
+		query += "\" WHERE ID = \"";
+		query += id;
+		query += "\";;
+
+	}
+	else {
+		query = "INSERT INTO";
+		query += targetTable;
+		query += " (ID, Color) VALUES(\"";
+		query += id;
+		query += "\", \"";
+		query += color;
+		query += "\");";
+	}
 
 	if (mysql_query(connectedDatebase, query.c_str()) != 0) {
 		cout << "Cannot Save Data" << endl;
